f22175e19fc9e3ab2fbba795f8773e897abe1fe3
Vrentadax <vrentadax@outlook.com>
2022-05-11 17:51:08 -0600

    added psuedo checklist to keep me on track and begun on models

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+// +++++++++++++++++ TO DO ++++++++++++++++++++++
+
+// WHEN I enter the command to invoke the application
+// THEN my server is started and the Sequelize models are synced to the MySQL database
+
+// WHEN I open API GET routes in Insomnia for categories, products, or tags
+// THEN the data for each of these routes is displayed in a formatted JSON
+
+// WHEN I test API POST, PUT, and DELETE routes in Insomnia
+// THEN I am able to successfully create, update, and delete data in my database
+
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
