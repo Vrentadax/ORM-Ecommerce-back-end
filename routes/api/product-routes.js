8db82ed6debc90b384fbbec7e176621a6950e3f6
Vrentadax <vrentadax@outlook.com>
2022-05-17 14:47:59 -0600

    added category-routes, cleaned product-routes a bit

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,7 +19,13 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbProductData => res.json(dbProductData))
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No products found!' });
+        return;
+      }
+      res.json(dbProductData);
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -47,7 +53,7 @@ router.get('/:id', (req, res) => {
   })
     .then(dbProductData => {
       if (!dbProductData) {
-        res.status(404).json({ message: 'No product found with this id' });
+        res.status(404).json({ message: 'No product found with this id!' });
         return;
       }
       res.json(dbProductData);
@@ -139,7 +145,7 @@ router.delete('/:id', (req, res) => {
   })
     .then(dbProductData => {
       if (!dbProductData) {
-        rs.status(404).json({ message: 'No product found with this id' });
+        rs.status(404).json({ message: 'No product found with this id!' });
         return;
       }
       res.json(dbProductData);
